dc6e21c3f337102530e5638f0b6d01c46a2f08c8
Sheikh Akhtar <SheikhA@pyramidconsultinginc.com>
2016-05-02 19:48:24 +0530

    more html manipulations

---
+++ scripts/filter1.js
@@ -0,0 +1,72 @@
+jQuery.noConflict();
+(function($){
+	//$("span:even").css({color:'pink'});
+	//$('span').filter(':even').css({color: 'red'});
+	$('span').filter(function(index){
+		return index%4==0;
+	}).css({color: 'red'});
+	
+	$("span:contains('Sachin')").css('color', 'blue');
+	$("span:contains('Sachin')").css({'text-decoration': 'underline'});	
+	
+	
+	$('span').filter(function(){
+		return $($(this)[0]).text().indexOf($('#searchText').val()) != -1 ;
+	}).css({'color':'pink'});
+	
+	
+	$('#cmdSearch').on('click', Search);
+	$('#cmdHtml').on('click', convertHmtl);
+	$('#cmdText').on('click',convertText);
+	$('#cmdAppend').on('click',dAppend);
+	$('#cmdAfter').on('click',dAfter);
+	$('#cmdAppendTo').on('click',dAppendTo);
+	$('#cmdPrependTo').on('click',dPrependTo);
+	
+	function dAppend(){
+		$('p').append("Append");
+	};
+	
+	function dPrependTo(){
+		$('span:eq(2)').prependTo('p');		
+	};
+	function dAppendTo(){
+		//$('p').appendTo("AppendTo");
+		//$('p').appendTo("AppendTo");
+		//$('<span>India</span>').appendTo('p');
+		$('span:eq(2)').appendTo('p');
+		
+		
+		
+	};
+	
+	function dAfter(){
+		$('p').after("After");
+	};
+	
+	function convertText(){
+		$('span').text(function(index, prev){
+			return prev.split('').reverse().join('') + index;
+		});
+		
+	};
+	
+	function Search(){
+		$('span').css({'color':'black'});
+		$('span').filter(function(){
+			return $($(this)[0]).text().indexOf($('#searchText').val()) != -1 ;
+		}).css({'color':'pink'});
+	};
+	
+	function convertHmtl(){
+		$('span').html(function(index, originalVal){
+			return "Earlier : " + originalVal + " and now : Nothing" + index;
+		});
+	};
+	
+	
+	
+	
+	//$("span:contains('Sachin')").css( "text-decoration", "underline" );
+	
+})(jQuery);
