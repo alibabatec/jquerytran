fe8e01debc46474857ff325eb4e6ac3c327601be
Sheikh Akhtar <SheikhA@pyramidconsultinginc.com>
2016-05-19 19:39:30 +0530

    ses

--- scripts/scrollmenu.js
+++ scripts/scrollmenu.js
@@ -1,7 +1,16 @@
 (function($){	
-	$('#dvScroll').scroll(function(event){
-		var menuOffset = $('#spMenu').offset().top + $(this).scrollTop() + 'px';
-		$('#spMenu').animate({
-			top: menuOffset}, {duration: 500, queue:false});
+	$(function(){
+		$('#dvScroll').scroll(function(event){
+		var menuOffset = $('#spMenu').offset().top + $(this).scrollTop() + "px";
+		//$('#spMenu').animate({top: menuOffset}, 'slow');	});
+	
+	
+		var div = $("spMenu");
+    	div.animate({top: menuOffset}, "slow");
+	
+});
+		
+		
 	});
+
 })(jQuery);
